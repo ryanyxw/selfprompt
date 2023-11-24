a636e5d686c8542042c1e8ecd40ccbf7dd95f938
Ryan Wang <ryanyxw@gmail.com>
2023-11-24 14:40:58 -0800

    Added evaluation scripts

--- src/evaluate_results_script.py
+++ src/evaluate_results_script.py
@@ -1,82 +1,15 @@
 import argparse
-from demonstrations.utils import get_instruction, setup_dataset, dummy_demonstration_formatting, esnli_demonstration_formatting, get_target_distribution
+from evaluations.utils import evaluate_json, get_json_path
 
 import os, sys
 #add the src of LLaMA-Efficient-Tuning to the path
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), os.path.join("LLaMA-Efficient-Tuning", "src")))
 
 def main(args):
+    path_to_results = get_json_path(args)
+    num_correct, tot_num = evaluate_json(path_to_results)
+    print(num_correct / tot_num)
 
-    new_dataset = setup_dataset(args)
-    instruction = get_instruction(args)
-    final_text = f"### Instruction:\n{instruction}\n\n"
-
-    if (args.exp_name == "dummy_rationale"):
-        target_distrib = get_target_distribution(args)
-        #extract demonstration until we've gotten one shot of each one
-        for i in range(len(new_dataset)):
-            if (target_distrib[new_dataset[i]["label"]] > 0):
-                target_distrib[new_dataset[i]["label"]] -= 1
-                final_text += dummy_demonstration_formatting(new_dataset[i]["premise"],
-                                                             new_dataset[i]["hypothesis"], new_dataset[i]["gold_label"])
-            if (target_distrib == [0, 0, 0]):
-                break
-    if (args.exp_name == "random_label"):
-        target_distrib = get_target_distribution(args)
-        # extract demonstration until we've gotten one shot of each one
-        found_premises = []
-        found_hypotheses = []
-        found_labels = []
-        found_rationales = []
-        for i in range(len(new_dataset)):
-            if (target_distrib[new_dataset[i]["label"]] > 0):
-                target_distrib[new_dataset[i]["label"]] -= 1
-                found_premises.append(new_dataset[i]["premise"])
-                found_hypotheses.append(new_dataset[i]["hypothesis"])
-                found_labels.append(new_dataset[i]["gold_label"])
-                found_rationales.append(new_dataset[i]["explanation_1"])
-            if (target_distrib == [0, 0, 0]):
-                break
-
-        # shuffle the found_labels and found_rationales in the same way
-        import random
-        random_array = list(range(len(found_labels)))
-        random.shuffle(random_array)
-
-        for i in range(len(found_labels)):
-            final_text += esnli_demonstration_formatting(found_premises[i], found_hypotheses[i],
-                                                         found_labels[i],
-                                                         found_rationales[random_array[i]])
-
-    if (args.exp_name == "random_rationale_and_label"):
-        target_distrib = get_target_distribution(args)
-        # extract demonstration until we've gotten one shot of each one
-        found_premises = []
-        found_hypotheses = []
-        found_labels = []
-        found_rationales = []
-        for i in range(len(new_dataset)):
-            if (target_distrib[new_dataset[i]["label"]] > 0):
-                target_distrib[new_dataset[i]["label"]] -= 1
-                found_premises.append(new_dataset[i]["premise"])
-                found_hypotheses.append(new_dataset[i]["hypothesis"])
-                found_labels.append(new_dataset[i]["gold_label"])
-                found_rationales.append(new_dataset[i]["explanation_1"])
-            if (target_distrib == [0, 0, 0]):
-                break
-
-        # shuffle the found_labels and found_rationales in the same way
-        import random
-        random_array = list(range(len(found_labels)))
-        random.shuffle(random_array)
-
-        for i in range(len(found_labels)):
-            final_text += esnli_demonstration_formatting(found_premises[i], found_hypotheses[i], found_labels[random_array[i]],
-                                                         found_rationales[random_array[i]])
-
-    #format into a python string variable
-    final_text = "demonstrations = " + '"""' + final_text + '"""'
-    print(final_text)
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -88,17 +21,10 @@ def parse_args():
     )
 
     parser.add_argument(
-        '--demonstration_pool',
+        '--num_score',
         required=True,
         type=int,
-        help="the range to sample from the test set"
-    )
-
-    parser.add_argument(
-        '--num_demonstrations',
-        required=True,
-        type=int,
-        help="the number of demonstrations to sample from the test set"
+        help="the number of examples to score"
     )
 
     parser.add_argument(
--- src/evaluations/utils.py
+++ src/evaluations/utils.py
@@ -1,5 +1,39 @@
-def evaluate_json(json_file):
+
+#assumes cwd is at bash script level (root)
+def get_json_path(args):
+    import os
+    return os.path.join("results_out_dir", args.exp_name, "generated_predictions.jsonl")
+
+def is_correct(prediction, label):
+    import re
+    def strip_non_alpha_from_end(s):
+        return re.sub(r'[^a-zA-Z]*$', '', s)
+
+    stripped_prediction = strip_non_alpha_from_end(prediction).lower()
+    stripped_label = label.lower() #assume the label is "neutral", "entailment" or "contradiction"
+
+    #we first check if the model has outputted its answer at the end (making sure that it is a single word)
+    if stripped_prediction.endswith(". " + stripped_label):
+        return True
+
+    #we next check if the correct label is present in the prediction (and none of the other two labels are present
+    label_options = ["neutral", "entail", "contradict"]
+    label_mapping = {"neutral": 0, "entailment": 1, "contradiction": 2}
+    present_arr = [l in stripped_prediction for l in label_options]
+    if (present_arr[label_mapping[stripped_label]] == 1 and sum(present_arr) == 1):
+        return True
+    return False
+
+
+def evaluate_json(path_to_results):
+
     import json
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    return data
+    num_correct = 0
+    tot_num = 0
+
+    with open(path_to_results, 'r') as f:
+        for line in f:
+            tot_num += 1
+            data = json.loads(line)
+            num_correct += is_correct(data["predict"], data["label"])
+    return num_correct, tot_num
